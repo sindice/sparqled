f9e7d1fde9d9a4a4cf8c0978d5f93fa688a590a2
scampi <stephane.campinas@deri.org>
2012-08-17 12:18:31 +0100

    Corrected the Debian package #9

--- sparql-summary/src/main/java/org/sindice/summary/rest/SummaryRest.java
+++ sparql-summary/src/main/java/org/sindice/summary/rest/SummaryRest.java
@@ -27,11 +27,11 @@ import java.util.ArrayList;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
